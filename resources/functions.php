75ec88b28315ea716ca98b2647ddd0035cbb87cd
Steven Polido <steven.polido@vistage.com>
2020-04-24 23:08:34 -0700

    Added SVG functionality

--- resources/functions.php
+++ resources/functions.php
@@ -90,3 +90,11 @@ Container::getInstance()
             'view' => require dirname(__DIR__).'/config/view.php',
         ]);
     }, true);
+
+function add_file_types_to_uploads($file_types){
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+    return $file_types;
+}
+add_filter('upload_mimes', 'add_file_types_to_uploads');
