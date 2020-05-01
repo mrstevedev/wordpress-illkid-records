c3e466362096984127cbbd779e9482eff637e6d7
Steven Polido <steven.polido@vistage.com>
2020-04-30 19:13:07 -0700

    Removed page methods

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <div class="<?php if(!is_front_page()) echo 'wrap container'; else echo '' ?>" role="document">
+    <div class="<?php if(is_product() ) echo 'container'; else echo 'wrap' ?>" role="document">
       <div class="content">
         <main class="main">
           @yield('content')
