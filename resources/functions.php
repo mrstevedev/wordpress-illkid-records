ede912a3c0bf259865a9b3000703c02e574a4155
Steven Polido <steven.polido@vistage.com>
2020-04-30 19:11:57 -0700

    Added Gutenberg to Woocommerce products

--- resources/functions.php
+++ resources/functions.php
@@ -109,3 +109,12 @@ function create_posttype() {
 }
 // Hook up function to theme setup
 add_action( 'init', 'create_posttype' );
+
+function wplook_activate_gutenberg_products($can_edit, $post_type){
+	if($post_type == 'product'){
+		$can_edit = true;
+	}
+	
+	return $can_edit;
+}
+add_filter('use_block_editor_for_post_type', 'wplook_activate_gutenberg_products', 10, 2);
