00cb8178cf2fe01cf44abf8dd661f89194a913d3
Steven Polido <steven.polido@vistage.com>
2020-05-01 15:57:52 -0700

    Added bootstrap classes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,10 @@
       <span class="logo-top">illkid</span>
       <span class="logo-bottom">records</span>
     </a>
-    <nav class="nav-primary">
+    <nav class="navbar navbar-expand-lg">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif      
