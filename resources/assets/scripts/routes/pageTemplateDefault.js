8dbc2067a7f55d9a3b3a59984e9e0c2e5ffc0b85
Steven Polido <steven.polido@vistage.com>
2020-05-05 21:27:45 -0700

    Removed custom route js file

--- resources/assets/scripts/routes/pageTemplateDefault.js
+++
@@ -1,9 +0,0 @@
-export default {
-    init() {
-      // scripts here run on the DOM load event
-      console.log('This is a page with the default template.');
-    },
-    finalize() {
-      // scripts here fire after init() runs
-    },
-  };
