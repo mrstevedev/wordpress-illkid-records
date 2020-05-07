a03f01a3ee1994e1412c455f1a500c11fee5a6c6
Steven Polido <steven.polido@vistage.com>
2020-05-06 21:21:00 -0700

    Removed button svg's

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,17 +2,10 @@
   <div class="cookies-notification-close">
     <p>THIS SITE USES COOKIES TO PROVIDE A GREAT USER EXPERIENCE. BY USING ILLKID RECORDS, YOU AGREE TO OUR USE OF COOKIES.</p>
     <button class="cookies__accept--btn btn">
-      <svg class="bi bi-check" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-      </svg>
       Accept
     </button>
     <a href="http://illkidrecords.local/cookie-policy">
       <button class="cookies__decline--btn btn">More 
-      <svg class="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L12.793 8l-2.647-2.646a.5.5 0 010-.708z" clip-rule="evenodd"/>
-        <path fill-rule="evenodd" d="M2 8a.5.5 0 01.5-.5H13a.5.5 0 010 1H2.5A.5.5 0 012 8z" clip-rule="evenodd"/>
-      </svg>
   </button>
 </a>
   </div>
