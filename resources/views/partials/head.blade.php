42e5c4b0e3b1661b40abbb8de00c8a98e69b0736
Steven Polido <steven.polido@vistage.com>
2020-07-16 16:19:38 -0700

    Moved preloaded fonts into if block

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,12 +3,15 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
-  <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBlackCond.woff" type="font/woff" crossorigin />
-  <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBlackCond.woff2" type="font/woff" crossorigin />
-  <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBold.woff" type="font/woff" crossorigin />
-  <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBold.woff2" type="font/woff" crossorigin />
-  <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueMedium.woff" type="font/woff" crossorigin />
-  <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueMedium.woff2" type="font/woff" crossorigin />
+  @if(!is_front_page()) 
+    <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBlackCond.woff" type="font/woff" crossorigin />
+    <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBlackCond.woff2" type="font/woff" crossorigin />
+    <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBold.woff" type="font/woff" crossorigin />
+    <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBold.woff2" type="font/woff" crossorigin />
+    <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueMedium.woff" type="font/woff" crossorigin />
+    <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueMedium.woff2" type="font/woff" crossorigin />
+  @endif
+
   <link rel="stylesheet" href="https://use.typekit.net/nlf5xci.css">
 
     @if(is_front_page()) 
