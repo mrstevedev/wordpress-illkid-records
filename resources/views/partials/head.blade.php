93537587abf1f98c086173835f9f13268afd5b47
Steven Polido <steven.polido@vistage.com>
2020-08-19 17:26:06 -0700

    Added meta description tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta property="og:image" content="@php bloginfo( 'url' ) @endphp/wp-content/uploads/2020/08/djtech3.jpg">
+  <meta name="description" content="San Diego based producer sp da iLL kid storefront for classic boom bap Hip-Hop. sp1200, mpc2000 and mad sample records.">
   @php wp_head() @endphp
   @if(!is_front_page()) 
     <link rel="preload" as="font" href="@php bloginfo('url') @endphp/wp-content/themes/illkidrecords/dist/fonts/HelveticaNeueBlackCond.woff" type="font/woff" crossorigin />
@@ -14,7 +16,7 @@
 
   <link rel="stylesheet" href="https://use.typekit.net/nlf5xci.css">
 
-    @if(is_front_page()) 
+  @if(is_front_page()) 
       <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     @endif
     <script src="https://kit.fontawesome.com/42562b750b.js" crossorigin="anonymous"></script>
