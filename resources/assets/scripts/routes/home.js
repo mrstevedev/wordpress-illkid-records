e4803e7e634d76946904d51d73d7a25513ebb736
Steven Polido <steven.polido@vistage.com>
2020-08-26 16:20:13 -0700

    Added scroll function

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -3,13 +3,24 @@ export default {
     // JavaScript to be fired on the home page
     $('[data-toggle="tooltip"]').tooltip()
 
-    $(window).on('scroll', function () {
-      if ($(this).scrollTop() > 100) {
-          $('header').addClass('not-transparent');
-      }
-      else {
-          $('header').removeClass('not-transparent');
-      }
+  //   $(window).on('scroll', function () {
+  //     if ($(this).scrollTop() > 100) {
+  //         $('header').addClass('not-transparent');
+  //     }
+  //     else {
+  //         $('header').removeClass('not-transparent');
+  //     }
+  // });
+
+   //sticky back to top
+   $(window).on('scroll', function(){
+    const scrollPos = $(window).scrollTop();
+    if(scrollPos >= 800){
+      $('.back-to-top').addClass('fixed animated fadeInRight show');
+        
+    } else if(scrollPos < 800){
+      $('.back-to-top').removeClass('fadeInRight show');
+    }
   });
 
 
